35188acbb0a06ba1f42d079cca22ec27462e87f1
eltech203 <eltech203@gmail.com>
2025-12-17 16:11:43 +0300

    sd

--- routes/wallet.routes.js
+++ routes/wallet.routes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const wallet = require("../controllers/wallet.controller");
 
 router.get("/:user_id", wallet.getWallet);
-router.get("/:user_id/ledger", wallet.getLedger);
+router.get("/get-ledger/:user_id", wallet.getLedger);
 router.get("/get-balance/:uid", wallet.getBalance);
 router.get("/get-wallet/:uid", wallet.getWalletByUid);
 

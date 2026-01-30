14b3cba6af3a710dec90bce48d665fa861e81c76
eltech203 <eltech203@gmail.com>
2026-01-30 22:07:24 +0300

    ASD

--- routes/wallet.routes.js
+++ routes/wallet.routes.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const wallet = require("../controllers/wallet.controller");
+const {getWallet,getLedger,getBalance,getWalletByUid} = require("../controllers/wallet.controller");
 
-router.get("/:user_id", wallet.getWallet);
-router.get("/get-ledger/:user_id", wallet.getLedger);
-router.get("/get-balance/:uid", wallet.getBalance);
-router.get("/get-wallet/:uid", wallet.getWalletByUid);
+router.get("/:user_id", getWallet);
+router.get("/get-ledger/:user_id", getLedger);
+router.get("/get-balance/:uid", getBalance);
+router.get("/get-wallet/:uid", getWalletByUid);
 
 
 module.exports = router;
